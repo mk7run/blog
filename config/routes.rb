800f2085f6b25aed833c7ab63412d21d6d3f6d00
Melissa Kanhirun <mk7run@gmail.com>
2017-10-29 17:08:20 -0500

    register and login users

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   root "posts#index"
 
   get '/about', to: 'pages#about'
+
+  resources :users, only: [:create, :new, :show ]
+  resources :sessions, only: [:new, :create, :destroy]
 end
